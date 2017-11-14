cc05bd56b020180d0a44f9bd25f1074a1604397e
Pertsev Alexey <oeermanz@gmail.com>
2017-11-15 01:32:30 +0300

    setup.py, travis config, gitignore

---
+++ README.rst
@@ -0,0 +1,7 @@
+Requests-testing
+=========
+
+.. image:: https://travis-ci.org/a-pertsev/requests-testing.svg?branch=master
+	:target: https://travis-ci.org/a-pertsev/requests-testing
+
+A utility library for mocking out the `requests` Python library.
---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+setup_requires = []
+
+if 'test' in sys.argv:
+    setup_requires.append('pytest')
+
+install_requires = [
+    'requests>=2.0',
+    'cookies',
+    'six'
+]
+
+if sys.version_info < (3, 2):
+    install_requires.append('mock')
+
+tests_require = [
+    'pytest',
+    'coverage >= 3.7.1, < 5.0.0',
+    'pytest-cov',
+    'pytest-localserver',
+    'flake8',
+    'idna',
+]
+
+extras_require = {
+    'tests': tests_require,
+}
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['test_requests_testing.py']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='requests-testing',
+    version='0.1',
+    author='Pertsev Alexey',
+    description='A utility library for mocking out the `requests` Python library.',
+    url='https://github.com/a-pertsev/requests-testing',
+    license='Apache 2.0',
+    long_description=open('README.rst').read(),
+    py_modules=['requests_testing', 'test_requests_testing'],
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    tests_require=tests_require,
+    setup_requires=setup_requires,
+    cmdclass={'test': PyTest},
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development'
+    ], )
