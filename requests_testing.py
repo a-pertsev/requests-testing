b778dd7af42535c2b0f730539ffa2af22e819bc2
Pertsev Alexey <oeermanz@gmail.com>
2017-11-15 00:34:06 +0300

    codebase

---
+++ requests_testing.py
@@ -0,0 +1,223 @@
+# coding: utf-8
+
+from __future__ import (absolute_import, print_function, division,
+                        unicode_literals)
+
+import idna
+from functools import wraps
+from collections import namedtuple
+from cookies import Cookies
+
+import six
+from requests.exceptions import ConnectionError
+from requests.adapters import HTTPAdapter
+from requests.utils import cookiejar_from_dict, requote_uri
+
+if six.PY2:
+    from urlparse import urlsplit, urlunparse, parse_qs
+else:
+    from urllib.parse import urlsplit, urlunparse, parse_qs
+
+if six.PY2:
+    try:
+        from six import cStringIO as BufferIO
+    except ImportError:
+        from six import StringIO as BufferIO
+else:
+    from io import BytesIO as BufferIO
+
+try:
+    from requests.packages.urllib3.response import HTTPResponse
+except ImportError:
+    from urllib3.response import HTTPResponse
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+
+Call = namedtuple('Call', ('request', 'response'))
+
+
+def _to_utf8_bytes(some):
+    if six.PY2 and not isinstance(some, str):
+        return some.encode('utf-8')
+    return some
+
+
+class _ParsedRequest(object):
+    def __init__(self, request_dict):
+        self.url, self.query_params = self.parse_url(request_dict.get('url'))
+        self.method = request_dict.get('method', 'GET')
+        self.headers = request_dict.get('headers', {})
+
+    def matches(self, real_request):
+        if not self._url_and_query_matches(real_request.url):
+            return False
+
+        if not self._method_matches(real_request.method):
+            return False
+
+        if not self._headers_matches(real_request.headers):
+            return False
+
+        return True
+
+    @staticmethod
+    def parse_url(url):
+        if url is None:
+            return None, {}
+
+        scheme, netloc, path, query, fragment = urlsplit(url)
+        host, _, port = netloc.partition(':')
+        host = str(idna.encode(host, uts46=True))
+        netloc = '{}:{}'.format(host, port) if port else host
+        path = '/' if not path else path
+        scheme, netloc, path, query, fragment = map(_to_utf8_bytes,
+                                                    (scheme, netloc, path, query, fragment))
+        parsed_query = parse_qs(query)
+        url = urlunparse((scheme, netloc, path, '', '', fragment))
+        url = requote_uri(url)
+        return url, parsed_query
+
+    def _url_and_query_matches(self, url):
+        if self.url is None:
+            return True
+
+        url, parsed_query = self.parse_url(url)
+        if self.url != url:
+            return False
+
+        for k, v in self.query_params.items():
+            if parsed_query.get(k) != v:
+                return False
+
+        return True
+
+    def _method_matches(self, method):
+        if self.method is None:
+            return True
+        return self.method == method
+
+    def _headers_matches(self, headers):
+        if self.headers is None:
+            return True
+
+        for header, value in self.headers.items():
+            if headers.get(header) != value:
+                return False
+
+        return True
+
+
+class MockedRequest(object):
+    def __init__(self, request, response=None, calls_limit=None):
+        if isinstance(request, six.string_types):
+            request = {'url': request}
+        if isinstance(response, six.string_types):
+            response = {'body': response}
+        self.request = _ParsedRequest(request)
+        self.response = response if response is not None else {}
+        self.calls_limit = calls_limit
+
+
+class Mock(object):
+    def __init__(self):
+        self.calls = []
+        self._mocked_requests = []
+        self._patcher = None
+        self._real_send = None
+
+    def _add_call(self, req, resp):
+        self.calls.append(Call(req, resp))
+
+    def add(self, request, response=None, calls_limit=1):
+        self._mocked_requests.append(MockedRequest(request, response, calls_limit))
+
+    def _find_match(self, request):
+        for i, mocked in enumerate(self._mocked_requests):
+            if mocked.request.matches(request):
+
+                if mocked.calls_limit is not None:
+                    mocked.calls_limit -= 1
+                    if mocked.calls_limit == 0:
+                        self._mocked_requests.pop(i)
+                return mocked
+
+    def _on_request(self, adapter, request, **kwargs):
+        mocked = self._find_match(request)
+
+        if not mocked:
+            error_msg = 'Connection refused: {} {}'.format(request.method, request.url)
+            response = ConnectionError(error_msg)
+            response.request = request
+            self._add_call(request, response)
+            raise response
+
+        if mocked.response.get('passthru'):
+            return self._real_send(adapter, request)
+
+        status = mocked.response.get('status', six.moves.http_client.OK)
+        body = mocked.response.get('body')
+        body = BufferIO(body.encode('utf-8') if body is not None else body)
+        response = HTTPResponse(
+            status=status,
+            reason=six.moves.http_client.responses.get(status),
+            body=body,
+            headers=mocked.response.get('headers'),
+            preload_content=False
+        )
+
+        response = adapter.build_response(request, response)
+        self._inject_cookies(request, response)
+        self._add_call(request, response)
+        return response
+
+    @staticmethod
+    def _inject_cookies(request, response):
+        try:
+            resp_cookies = Cookies.from_request(request.headers['set-cookie'])
+            response.cookies = cookiejar_from_dict({
+                v.name: v.value for _, v in resp_cookies.items()
+            })
+        except (KeyError, TypeError):
+            pass
+
+    def _stop(self):
+        self._patcher.stop()
+        self.calls[:] = []
+        self._mocked_requests = []
+
+    def _start(self):
+        self._real_send = HTTPAdapter.send
+        self._patcher = mock.patch('requests.adapters.HTTPAdapter.send',
+                                   lambda *a, **kw: self._on_request(*a, **kw))
+        self._patcher.start()
+
+    def __enter__(self):
+        self._start()
+        return self
+
+    def __exit__(self, ex_type, value, traceback):
+        success = ex_type is None
+        self._stop()
+        return success
+
+
+_mock = Mock()
+
+
+def activate(fn):
+    @wraps(fn)
+    def new_fn(*args, **kwargs):
+        with _mock:
+            return fn(*args, **kwargs)
+
+    return new_fn
+
+
+add = _mock.add
+calls = _mock.calls
+
+__all__ = ['add', 'calls', 'activate']
---
+++ test_requests_testing.py
@@ -0,0 +1,185 @@
+# coding: utf-8
+
+from __future__ import (absolute_import, print_function, division,
+                        unicode_literals)
+
+from requests.exceptions import ConnectionError
+
+import pytest
+import requests
+import requests_testing
+
+
+@requests_testing.activate
+def test_simple_body():
+    requests_testing.add(request={'url': 'http://example.com'}, response={'body': 'ok'})
+    resp = requests.get('http://example.com')
+
+    assert resp.text == 'ok'
+    assert len(requests_testing.calls) == 1
+    assert requests_testing.calls[0].request.url == 'http://example.com/'
+
+
+@requests_testing.activate
+def test_default_error():
+    requests_testing.add(request={'url': 'http://example.com'})
+
+    with pytest.raises(ConnectionError):
+        requests.get('http://example.com/foo')
+
+    assert len(requests_testing.calls) == 1
+    assert requests_testing.calls[0].request.url == 'http://example.com/foo'
+    assert type(requests_testing.calls[0].response) is ConnectionError
+    assert requests_testing.calls[0].response.request
+
+
+@requests_testing.activate
+def test_arbitrary_status_code():
+    url = 'http://example.com/'
+    requests_testing.add(request={'url': url}, response={'body': 'test', 'status': 418})
+    resp = requests.get(url)
+    assert resp.status_code == 418
+    assert resp.reason is None
+
+
+@requests_testing.activate
+def test_response_cookies():
+    body = 'test'
+    status = 200
+    headers = {'set-cookie': 'session_id=12345; a=b; c=d'}
+    url = 'http://example.com/'
+
+    requests_testing.add(request={'url': url, 'headers': headers}, response={'body': body})
+    resp = requests.get(url, headers=headers)
+    assert resp.text == body
+    assert resp.status_code == status
+    assert 'session_id' in resp.cookies
+    assert resp.cookies['session_id'] == '12345'
+    assert resp.cookies['a'] == 'b'
+    assert resp.cookies['c'] == 'd'
+
+
+@requests_testing.activate
+def test_arbitrary_headers():
+    body = 'test'
+    status = 200
+    req_headers = {'some-header': 'request'}
+    mismatch_headers = {'some': 'request'}
+    response_headers = {'some-header': 'response'}
+    url = 'http://example.com/'
+
+    requests_testing.add(request={'url': url, 'headers': req_headers},
+                         response={'body': body, 'headers': response_headers})
+
+    with pytest.raises(ConnectionError):
+        requests.get('http://example.com/', headers=mismatch_headers)
+
+    resp = requests.get(url, headers=req_headers)
+    assert resp.text == body
+    assert resp.status_code == status
+    assert resp.headers == response_headers
+
+
+@requests_testing.activate
+def test_handles_unicode_url():
+    url = u'http://www.संजाल.भारत/hi/वेबसाइट-डिजाइन'
+    requests_testing.add(request={'url': url}, response={'body': 'ok'})
+    resp = requests.get(url)
+
+    assert resp.text == 'ok'
+    assert len(requests_testing.calls) == 1
+
+
+@requests_testing.activate
+def test_handles_unicode_querystring():
+    url = u'http://example.com/test?type=2&ie=utf8&query=汉字'
+    requests_testing.add(request={'url': url}, response={'body': 'ok'})
+    resp = requests.get(url)
+    assert resp.text == 'ok'
+
+
+@requests_testing.activate
+def test_query_mismatch():
+    url = u'http://example.com/test?type=2&ie=utf8&query=汉字'
+    mismatch_url = u'http://example.com/test?type=2&ie=utf8&query=汉'
+    requests_testing.add(request={'url': mismatch_url}, response={'body': 'ok'})
+
+    with pytest.raises(ConnectionError):
+        requests.get(url)
+
+
+methods = [('GET', requests.get), ('POST', requests.post), ('PUT', requests.put)]
+
+
+@pytest.mark.parametrize('method_name, method', methods)
+def test_http_method_match(method_name, method):
+    @requests_testing.activate
+    def run():
+        url = u'http://example.com/test'
+        requests_testing.add(request={'url': url, 'method': method_name}, response={'body': 'ok'})
+        resp = method(url)
+        assert resp.text == 'ok'
+    run()
+
+
+@requests_testing.activate
+def test_multiple_urls():
+    requests_testing.add(request={'url': 'http://example.com/one'}, response={'body': 'one ok'})
+    requests_testing.add(request={'url': 'http://example.com/two'}, response={'body': 'two ok'})
+
+    resp = requests.get('http://example.com/one')
+    assert resp.text == 'one ok'
+
+    resp = requests.get('http://example.com/two')
+    assert resp.text == 'two ok'
+
+
+@requests_testing.activate
+def test_default_calls_limit():
+    requests_testing.add(request={'url': 'http://example.com/one'})
+
+    requests.get('http://example.com/one')
+
+    with pytest.raises(ConnectionError):
+        requests.get('http://example.com/one')
+
+
+@requests_testing.activate
+def test_default_calls_limit_param():
+    requests_testing.add(request={'url': 'http://example.com/one'}, calls_limit=2)
+
+    requests.get('http://example.com/one')
+    requests.get('http://example.com/one')
+
+    with pytest.raises(ConnectionError):
+        requests.get('http://example.com/one')
+
+
+@requests_testing.activate
+def test_multiple_responses():
+    requests_testing.add(request={'url': 'http://example.com'}, response={'body': 'one ok'})
+    requests_testing.add(request={'url': 'http://example.com'}, response={'body': 'two ok'})
+
+    resp = requests.get('http://example.com')
+    assert resp.text == 'one ok'
+
+    resp = requests.get('http://example.com')
+    assert resp.text == 'two ok'
+
+
+@requests_testing.activate
+def test_sugar_arguments():
+    requests_testing.add('http://example.com/one', 'one ok')
+    resp = requests.get('http://example.com/one')
+    assert resp.text == 'one ok'
+
+
+def test_passthru(httpserver):
+    httpserver.serve_content('local OK', headers={'Content-Type': 'text/plain'})
+
+    @requests_testing.activate
+    def run():
+        requests_testing.add(request={'url': httpserver.url}, response={'passthru': True})
+        resp = requests.get(httpserver.url)
+        assert resp.text == 'local OK'
+    run()
